a71d11baf2eaf7afc5e2e5a5cb2d4a97bc700ee4
Lorenzo Navarro <lorenzonavarro@ieee.org>
2024-10-06 17:00:33 -0500

    (feat) product service added

---
+++ src/services/productApi.js
@@ -0,0 +1,40 @@
+export async function getProducts (){
+    try {
+        const response = await fetch(`https://dummyjson.com/products`);
+        const data = await response.json();
+        console.log(data)
+        return data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export async function getProductByName(name){
+    try{
+        const response = await fetch(`https://dummyjson.com/products/search?=${name}`);
+        const data = response.json();
+        return data;
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export async function getSingleProduct(id){
+    try{
+        const response = await fetch(`https://dummyjson.com/products/${id}`);
+        const data = response.json();
+        return data;
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export async function getAndSkipProducts(limit,skip){
+    try{
+        const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
+        const data = response.json();
+        return data;
+    }catch(error){
+        console.log(error)
+    }
+}
