3a0642aa03b12a38c4850464e68d8f710fc8912c
Lorenzo Navarro <lorenzonavarro@ieee.org>
2024-10-06 20:33:20 -0500

    (feat) product grid implemented

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import { getProducts } from './src/services/productApi.js'
+import { getAndSkipProducts, getProducts } from './src/services/productApi.js'
+
+
+const products = await getProducts();
+const grid = document.getElementById('list-id')
+
+products.products.forEach((element) => {
+  let div = document.createElement('div');
+  
+  //image
+  const imageItem = document.createElement('img')
+  imageItem.src = element.thumbnail
+  div.appendChild(imageItem)
+
+  const content = document.createTextNode(element.title);
+  div.appendChild(content);
+
+  //description
+  const descriptionItem = document.createElement('p')
+  const description = document.createTextNode(element.description)
+  div.appendChild(description)
+
+
+  //price
+  // const priceItem = document.createElement('p')
+  // const price = document.
+
+  grid.appendChild(div)
+})
 
