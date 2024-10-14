a74ce3661b9da00e5c94256678a68d817afb0569
Lorenzo Navarro <lorenzonavarro@ieee.org>
2024-10-13 22:50:06 -0500

    (feat) initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HomeScreen from './pages/HomeScreen'
+import Layout from './shared/Layout/Layout'
+
+function App() {
+
+  return (
+    <Layout>
+      <HomeScreen />
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {path: '/', element: <App/>}
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+)
