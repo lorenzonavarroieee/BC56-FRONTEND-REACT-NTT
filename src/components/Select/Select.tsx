c7e71e3eb8719112ccc89e9fff569335fa980d25
Lorenzo Navarro <lorenzonavarro@ieee.org>
2024-10-13 23:42:53 -0500

    (fix) home screen changed

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,14 @@
-interface SelectProps{}
+import { Category } from "../../entities/category"
 
-export default function SelectComponent({}: SelectProps){
+interface SelectProps{
+    categories: Category[];
+    handleChange: () => {};
+}
+
+export default function SelectComponent({categories, handleChange}: SelectProps){
+    <div className="input-field">
+        <select value="" onChange={handleChange}>
+            {categories.map((e,index) => <option key={index} value={e.slug}>{e.slug}</option>)}
+        </select>
+    </div>
 }
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -4,11 +4,12 @@ import { Product } from "../entities/product";
 import { limitAndSkipProducts } from "../services/products/product-service";
 import Card from "../components/Card/Card";
 import TextInputComponent from "../components/TextField/TextField";
+import { Category } from "../entities/category";
+import { getCategories } from "../services/categories/category-service";
 
 function HomeScreen(){
     const [products, setProducts] = useState<Product[]>([]);
-    
-
+    const [categories, setCategories] = useState<Category[]>([]);
     const [page, setPage] = useState<number>(0);
 
     const fetchProducts = async () => {
@@ -18,31 +19,51 @@ function HomeScreen(){
         }catch(error){
             throw new Error();
         }
-
     }
 
-    const searchProduct = () => {}
+    const fetchCategories = async () => {
+        try {
+            const data = await getCategories();
+            const filtered = data.filter((item) => item.slug);
+            setCategories(filtered);
+        }catch(error) {
+            throw new Error();
+        }
+    }
 
-    const buyItem = () => {}
+    // const searchProduct = () => {}
+
+    // const buyItem = () => {}
+
+    // const handleChangeByCategory = async() => {}
 
     
-    const handleSetPage = () => {
+    const handleSetNextPage = () => {
         setPage(prev => prev + 1);
+    }
+
+    const handleSetPrevPage = () => {
+        setPage(prev => prev - 1);
     }
 
     useEffect(() => {
         fetchProducts();
     },[page])
 
+    useEffect(() => {
+        fetchCategories();
+    },[])
+
     return(
         <div>
             <div>
-                <TextInputComponent placeholder="Search your product" onKeyPress={searchProduct} />
-                <button onClick={handleSetPage}>Next Page</button>
+                <TextInputComponent placeholder="Search your product" onKeyPress={() => {}} />
+                <button onClick={handleSetPrevPage}>Prev Page</button>
+                <button onClick={handleSetNextPage}>Next Page</button>
             </div>
             <div className="align-items">
                 <div className="products-grid">
-                    {products && products.map((product) => <Card key={product.id} product={product} onPress={buyItem}/> )}
+                    {products && products.map((product) => <Card key={product.id} product={product} onPress={() => {}}/> )}
                 </div>
             </div>
         </div>
